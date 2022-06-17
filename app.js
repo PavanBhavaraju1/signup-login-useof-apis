ce5f8c828ab1fa07e672f233a0946bcc1e7a669e
PavanBhavaraju <bhavarajupavan107@gmail.com>
2022-06-17 18:05:23 +0530

    diff

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 
 
